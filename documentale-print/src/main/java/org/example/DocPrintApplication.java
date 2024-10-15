f35c2366c35f096ab0d5ad60cfc62ee9c07be187
Francesco Barbierato <f.barbierato@almaviva.it>
2024-10-15 20:40:49 +0200

    Print and archive config + startup

--- documentale-print/src/main/java/org/example/DocPrintApplication.java
+++ documentale-print/src/main/java/org/example/DocPrintApplication.java
@@ -2,10 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class DocPrintApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocPrintApplication.class, args);
