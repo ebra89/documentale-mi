c19d7064010b01100ff8e54406167abd714814e6
Francesco Barbierato <f.barbierato@almaviva.it>
2024-10-15 20:15:49 +0200

    New MS: print

---
+++ documentale-print/src/main/java/org/example/DocPrintApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DocPrintApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocPrintApplication.class, args);
+    }
+}
---
+++ documentale-print/src/main/java/org/example/controller/DocPrintController.java
@@ -0,0 +1,31 @@
+package org.example.controller;
+
+//import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.example.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@Tag(name = "TestController - TAG", description = "Descrizione - TestController - TAG")
+public class DocPrintController {
+
+    private final TestService service;
+
+    @Autowired
+    public DocPrintController(TestService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    //@Tag(name = "metodo get", description = "descrizione metodo get")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<>(service.getTestMessage(), HttpStatus.OK);
+    }
+
+}
