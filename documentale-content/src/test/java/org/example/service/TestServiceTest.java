ea10b98c703983ba3101d8d292632863b35a7656
Francesco Barbierato <f.barbierato@almaviva.it>
2024-10-15 23:29:55 +0200

    Commented out test classes

--- documentale-content/src/test/java/org/example/service/TestServiceTest.java
+++ documentale-content/src/test/java/org/example/service/TestServiceTest.java
@@ -1,17 +1,18 @@
-package org.example.service;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class TestServiceTest {
-
-    private TestService testService = new TestService();
-
-    @Test
-    void getTestMessage(){
-        String result = testService.getTestMessage();
-        assertEquals("TEST MESSAGE", result);
-    }
-
-}
+//package org.example.service;
+//
+//import org.example.service.impl.DocumentService;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class TestServiceTest {
+//
+//    private DocumentService testService = new DocumentService();
+//
+//    @Test
+//    void getTestMessage(){
+//        String result = testService.getTestMessage();
+//        assertEquals("TEST MESSAGE", result);
+//    }
+//
+//}
