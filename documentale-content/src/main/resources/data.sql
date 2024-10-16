3d43fadaa31e212c0e4b11365ad653ef04d18e6f
Francesco Barbierato <f.barbierato@almaviva.it>
2024-10-17 01:18:21 +0200

    Deleted half document init data

--- documentale-content/src/main/resources/data.sql
+++ documentale-content/src/main/resources/data.sql
@@ -10,16 +10,7 @@ INSERT INTO document (ref_number, owner, type, state, location, doc_date, create
 VALUES ('D0001', 'Giovanni', 'TA', 'received', 'MI', '2024-10-12', '2024-10-12', 'admin', '2024-10-12', 'admin', 1);
 
 INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
-VALUES ('D0002', 'Marco', 'TB', 'received', 'MI', '2024-10-13', '2024-10-13', 'Mario Rossi', '2024-10-13', 'Mario Rossi', 1);
-
-INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
-VALUES ('D0003', 'Marco', 'TA', 'received', 'MI', '2024-10-14', '2024-10-14', 'Luca Verdi', '2024-10-14', 'Mario Rossi', 1);
-
-INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
 VALUES ('D0004', 'Chiara', 'TB', 'received', 'MI', '2024-10-12', '2024-10-12', 'Mario Rossi', '2024-10-12', 'Mario Rossi', 2);
-
-INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
-VALUES ('D0005', 'Giovanni', 'TA', 'verify', 'MI', '2024-10-12', '2024-10-12', 'admin', '2024-10-13', 'Alice Bianchi', 2);
 
 INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
 VALUES ('D0006', 'Giovanni', 'TB', 'verify', 'MI', '2024-10-13', '2024-10-13', 'Luca Verdi', '2024-10-14', 'Mario Rossi', 2);
@@ -29,9 +20,3 @@ VALUES ('D0007', 'Anna', 'TA', 'archived', 'MI', '2024-10-13', '2024-10-13', 'Ma
 
 INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
 VALUES ('D0008', 'Anna', 'TB', 'archived', 'MI', '2024-10-14', '2024-10-14', 'Alice Bianchi', '2024-10-14', 'Luca Verdi', 1);
-
-INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
-VALUES ('D0009', 'Pietro', 'TA', 'archived', 'MI', '2024-10-13', '2024-10-13', 'Mario Rossi', '2024-10-13', 'Luca Verdi', 2);
-
-INSERT INTO document (ref_number, owner, type, state, location, doc_date, create_date, create_user, update_date, update_user, protocol_id) 
-VALUES ('D0010', 'Marco', 'TB', 'archived', 'MI', '2024-10-14', '2024-10-14', 'Alice Bianchi', '2024-10-14', 'Luca Verdi', 2);
